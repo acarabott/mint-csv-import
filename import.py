a21aa71d1dcc729f2cce42cf2196b4b3f65f167f
Arthur Carabott <arthur@arthurcarabott.com>
2021-12-11 22:32:51 -0800

    trim trailing whitespace

--- import.py
+++ import.py
@@ -7,38 +7,38 @@ from decimal import Decimal
 
 """
 #################################
-Overview: 
+Overview:
 #################################
 
-Simulates bulk manual transaction adds to mint.com. Mint manual transactions are submitted as "cash transactions" which 
-will mean it shows in your cash / all accounts transaction list. You cannot submit manual transactions against credit 
-cards or other integrated bank accounts (even in Mint's UI this is not possible and ends up as cash transaction). 
+Simulates bulk manual transaction adds to mint.com. Mint manual transactions are submitted as "cash transactions" which
+will mean it shows in your cash / all accounts transaction list. You cannot submit manual transactions against credit
+cards or other integrated bank accounts (even in Mint's UI this is not possible and ends up as cash transaction).
 
-Approach Credits: 
+Approach Credits:
 Simulating manual transactions from UI is based on Nate H's proof of concept from https://www.youtube.com/watch?v=8AJ3g5JGmdU
 
 Python Credits:
 Credit to https://github.com/ukjimbow for his work on Mint imports for UK users in https://github.com/ukjimbow/mint-transactions
 
-Process Documentation: 
+Process Documentation:
 1. Import CSV
-2. Process date for correct format and HTTP encode result 
+2. Process date for correct format and HTTP encode result
 3. Process merchant for HTTP encode
 4. Process categories change your banks category name into a mint category ID (limited in scope based on the categories
 5 needed when I wrote this)
-6. Process amount for positive or negative value indicating income or expense 
-7. Send POST Request to mint as new transaction. 
+6. Process amount for positive or negative value indicating income or expense
+7. Send POST Request to mint as new transaction.
 8. Force Randomized Wait Time before starting next request
 
 Future Development:
-1. Replace curl command string generation with parametrized curl class constructor 
+1. Replace curl command string generation with parametrized curl class constructor
 2. Add support for the rest of the manual transaction items
 
 """
 
 """
 #################################
-Settings 
+Settings
 #################################
 """
 verbose_output = 1  # should verbose messages be printed [0,1]
@@ -52,10 +52,10 @@ csv_paths = [
 
 """
 #################################
-Mint Client Credentials 
+Mint Client Credentials
 #################################
 
-You will need the tags, cookie, and token to simulate a UI form submission. You can get these by opening developer tools > network analysis tab and doing 
+You will need the tags, cookie, and token to simulate a UI form submission. You can get these by opening developer tools > network analysis tab and doing
 a test submission in mint.com. From there look for the post request to "updateTransaction.xevent" and grab the credentials from the header and body
 """
 account = 'XXXXXXX'  # grab from POST request form body in devtools
@@ -68,7 +68,7 @@ token = 'XXXXXXX'  # grab from POST request form body in devtools
 
 """
 #################################
-Import CSV using the pythons csv reader 
+Import CSV using the pythons csv reader
 #################################
 """
 
@@ -264,11 +264,11 @@ for csv_name in csv_paths:
 
             """
             #################################
-            Process Categories 
+            Process Categories
             #################################
-        
-            Support is limited to the categories I needed at the time, if you need to map more you can. To get category ids: 
-            1. Go to mint 
+
+            Support is limited to the categories I needed at the time, if you need to map more you can. To get category ids:
+            1. Go to mint
             2. Add a transactions
             3. Right click "inspect-element" on the category you want
             4. The ID is in the <li> item that encapsulates the a href
@@ -288,7 +288,7 @@ for csv_name in csv_paths:
 
             """
             #################################
-            Process Amount seeing if transaction is an expense or income.   
+            Process Amount seeing if transaction is an expense or income.
             #################################
             """
             if amount < 0:
@@ -299,7 +299,7 @@ for csv_name in csv_paths:
             """
             #################################
             Build CURL POST Request
-            TODO: Swap command string generation for parametrized curl class 
+            TODO: Swap command string generation for parametrized curl class
             #################################
             """
 
